41829aa8d1912b012e65536a1454b3ceb304f4f9
Do Minh Duy <dominhduy09@gmail.com>
2026-01-15 20:46:24 +0000

    initial

---
+++ lab01/hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// single comment
+
+/*
+block comment
+*/
+
+/*
+public static void main (String[] args) {}
+*/
+
+int main() {
+    printf("hello, world\n");
+    return 0;
+}
