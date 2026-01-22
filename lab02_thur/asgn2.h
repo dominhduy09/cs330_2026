9d91edfc27a2d4efa1cc6cbeb7af50edc360a624
Do Minh Duy <dominhduy09@gmail.com>
2026-01-22 20:50:54 +0000

    add Collatz function

---
+++ lab02_thur/asgn2.h
@@ -0,0 +1,45 @@
+#ifndef __asgn2__
+#define __asgn2__
+
+/* the two lines above check to ensure
+we haven't already included this header*/
+
+
+/* your functions go here */
+// Note: main() goes in the asgn2.c file
+
+/*
+Collatz Conjecture
+
+n ==> even: n/2
+    odd: 3(n) + 1
+
+    10/2    3(5) + 1    16/2    8/2    4/2    2/2    1
+    5       16          8       4      2      1
+
+*/
+
+/*
+descr: this function prints the Collatz sequence for n
+takes: int n, to calc the Collatz
+return: nothing, just prints Collatz as side effect
+*/
+
+void printCollatz(int n) {
+    // loop
+    while (n != 1)
+    {
+        if (n % 2 == 0) {
+            n = n / 2;
+        }
+
+        else {
+            n = 3 * n + 1;
+        }
+
+        printf("%d ", n);
+    }
+    printf("\n");
+} // end printCollatz
+
+#endif
---
+++ lab02_thur/lab02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "asgn2.h"
+
+int main() {
+    printf("hello, world\n");
+
+    for(int i = 2; i <= 10; i++) {
+        printf("This is the Collatz sequence for %d: ", i);
+        printCollatz(i);
+    }
+
+    return 0;
+}
